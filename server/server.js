dc84621bc8a0e0a4a43f370d87d96cfa8f45d849
Jake Girhiny <jegirhiny@gmail.com>
2024-07-21 11:31:11 -0400

    add cors to route

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const path = require("path");
+const cors = require("cors");
 const app = express();
 
 app.use(express.json());
 
+app.use(cors());
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 app.use("/user", require("./routes/userRoute"));
