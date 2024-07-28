10c75d8bd1a430a98eef35fb62a7ac14bd51c3c7
Jake Girhiny <jegirhiny@gmail.com>
2024-07-28 16:01:35 -0400

    update

--- client/src/components/item-page/item-page.component.jsx
+++ client/src/components/item-page/item-page.component.jsx
@@ -81,7 +81,7 @@ const ItemPage = ({ triggerRender, setTriggerRender }) => {
   return (
     <div className="item-page">
       <img
-        src={`${process.env.REACT_APP_URI}${item.images[0].path}`}
+        src={`${process.env.REACT_APP_URI}/opt/render/project/src${item.images[0].path}`}
         alt={item.images[0].name}
         className="item-page-img"
       />
