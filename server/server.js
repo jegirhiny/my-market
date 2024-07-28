b7f2a1230e270f24b8b91521d4e4a49c9ba97766
Jake Girhiny <jegirhiny@gmail.com>
2024-07-28 15:35:18 -0400

    update

--- server/server.js
+++ server/server.js
@@ -1,11 +1,12 @@
 const express = require("express");
+const path = require("path");
 const cors = require("cors");
 const app = express();
 
 app.use(express.json());
 
 app.use(cors());
-app.use("/uploads", express.static("/mnt/data/images"));
+app.use("/uploads", express.static(path.join(__dirname, "/mnt/data/images")));
 
 app.use("/user", require("./routes/userRoute"));
 app.use("/item", require("./routes/itemRoute"));
