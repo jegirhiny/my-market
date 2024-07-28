93315c226daf63aab9d4048dd3d45b8f6d08cb67
Jake Girhiny <jegirhiny@gmail.com>
2024-07-28 11:09:55 -0400

    fix multer

--- server/routes/itemRoute.js
+++ server/routes/itemRoute.js
@@ -14,8 +14,6 @@ const {
   removeCartItem,
 } = require("../controller/itemController");
 
-const multer = require("multer");
-
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, "/mnt/data/images");
