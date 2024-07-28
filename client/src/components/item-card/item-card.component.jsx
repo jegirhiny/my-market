7bc11e425dc46788f8ad13681ea14f092f6b4805
Jake Girhiny <jegirhiny@gmail.com>
2024-07-28 15:54:27 -0400

    update

--- client/src/components/item-card/item-card.component.jsx
+++ client/src/components/item-card/item-card.component.jsx
@@ -40,7 +40,7 @@ const ItemCard = ({ item, setKey }) => {
   return (
     <div className="item-card" onClick={() => navigate(`/item/${item.id}`)}>
       <img
-        src={`${process.env.REACT_APP_URI}/${item.images[0].path}`}
+        src={`${process.env.REACT_APP_URI}${item.images[0].path}`}
         alt={item.images[0].name}
         className="item-img"
       />
